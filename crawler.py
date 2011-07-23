54e02bb9592a0e85e828244c60f2f0250f3fa80a
nvasilakis <nikos.ailo@gmail.com>
2011-07-23 22:10:23 +0300

    Finalizing a working prototype

--- crawler.py
+++ crawler.py
@@ -15,38 +15,50 @@ class Crawler:
         #links = [[i, "", ] for i in seed]
 
     def crowl(self):
+        """ The crawling is done in two phases after the URL pops from the queue
+        1. Add URL with some info to notes (if source is more than 40k chars incl. html)
+        2. Push children URLs to queue if they pass all crawling validations
+
+        """
         link = self.queue.pop()
         #for link in nea:
         #one_level
-        f = open("crowler.log", "a")
+        f = open("crawler.log", "a")
         f.write(link)
         usock = urllib.urlopen(link)
         parser = Parser()
         source = usock.read()
-        if source.__len__() > 10:
+        if source.__len__() > 1:
             f.write(" [" + str(source.__len__()) + "]  * \n")
+            self.notes[link]="working"
         parser.feed(source) # parses all the html source
         parser.close()
         usock.close()
+        f.write("-----")
         for url in parser.urls:
-            #make use of regular expressions
+
             if 'http' not in url:
-                url = link + url
-            if url not in self.queue:
-                ###8a kanw push ta link pou prepei
+               url = link + url
+            if url not in self.queue and url not in self.notes.keys():
                 self.queue.append(url)
-                f.write(url + "\n")
+                #f.write(url + "\n")
         f.close()
 
     def status(self):
        print "\n".join(["%s" % (k,) for k in self.queue])
        # the same for notes [["%s: %s" % (k,v) for k,v in notes.items()]
+       print "\n\n --------- \n\n"
+       print "\n".join(["%s: %s" % (k,v) for k,v in self.notes.items()])
+
+    def keep_crawling(self):
+        # change length of notes to adjust time of execution
+        if self.notes.__len__() < 10:
+            return True;
 
 
 if __name__ == "__main__":
-    seed = ["http://www.ceid.upatras.gr/", "http://www.catonmat.net/"] # "http://www.catb.org/~esr/",
+    seed = ["http://www.ceid.upatras.gr/", "http://www.catonmat.net"] # "http://www.catb.org/~esr/",
     spider = Crawler(seed)
-    spider.crowl()
-    spider.status()
-    spider.crowl()
+    while spider.keep_crawling():
+        spider.crowl()
     spider.status()
