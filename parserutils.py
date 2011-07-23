7fd3be2d43e3bcb8cc55106d63c44dc8330986f1
nvasilakis <nikos.ailo@gmail.com>
2011-07-23 17:04:43 +0300

    Adding the parseutils lib, a helper class for extracting URLs

---
+++ parserutils.py
@@ -0,0 +1,25 @@
+"""Extract list of URLs in a web page
+
+This library is used to extract URLs from web pages. It is part of the language
+processing project and directly used in the crowler class
+"""
+from sgmllib import SGMLParser
+
+class Parser(SGMLParser):
+	def reset(self):
+		SGMLParser.reset(self)
+		self.made = []
+		self.urls = []
+
+    # What you may not have realized before is that getattr will find
+    # methods defined in descendants of an object as well as the object
+    # itself.
+	def start_a(self, attrs):
+		href = ["%s" % v  for k, v in attrs if k=='href' and 'mailto' not in v]
+		if href:
+			self.urls.extend(href)
+
+	def start_link(self, attrs):
+		href = ["%s ++ %s" % (k,v) for k, v in attrs if k=='href']
+		if href:
+			self.made.extend(href)
