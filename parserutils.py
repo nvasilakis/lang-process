ec7a0ebf55923d484114d4c20f7802be5dda4d54
nvasilakis <nikos.ailo@gmail.com>
2011-08-11 18:53:57 +0300

    Extending parser to handle and export simple text

--- parserutils.py
+++ parserutils.py
@@ -10,18 +10,19 @@ class Parser(SGMLParser):
 		SGMLParser.reset(self)
 		self.made = []
 		self.urls = []
+		self.text = []
 
-    # Defining a number of filters in order to check if a link is valid
-    # for addition in the URL queue
+	# Defining a number of filters in order to check if a link is valid
+	# for addition in the URL queue
 	def start_a(self, attrs):
 		href = ["%s" % v  for k, v in attrs if k=='href' and
-                'mailto' not in v and
-                '.com' in v and 'acm.org' not in v and
-                'wikipedia' not in v and '.gov' not in v and
-                '.pdf' not in v and '.doc' not in v and
-                '.xls' not in v and '.php' not in v and
-                '.cgi' not in v and '.js' not in v
-        ]
+				'mailto' not in v and
+				'.com' in v and 'acm.org' not in v and
+				'wikipedia' not in v and '.gov' not in v and
+				'.pdf' not in v and '.doc' not in v and
+				'.xls' not in v and '.php' not in v and
+				'.cgi' not in v and '.js' not in v
+		]
 		if href:
 			self.urls.extend(href)
 
@@ -29,3 +30,6 @@ class Parser(SGMLParser):
 		href = ["%s ++ %s" % (k,v) for k, v in attrs if k=='href']
 		if href:
 			self.made.extend(href)
+
+	def handle_data(self, text):
+		self.text.extend(text)
