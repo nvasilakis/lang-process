4b29b796730075e17b0e0a25d5bb4772efbdd43d
nvasilakis <nikos.ailo@gmail.com>
2011-08-11 20:34:31 +0300

    Using HTMLParser instead of SGMLParser

--- parserutils.py
+++ parserutils.py
@@ -3,11 +3,12 @@
 This library is used to extract URLs from web pages. It is part of the language
 processing project and directly used in the crawler class
 """
-from sgmllib import SGMLParser
+# from sgmllib import SGMLParser
+from HTMLParser import HTMLParser
 
-class Parser(SGMLParser):
+class Parser(HTMLParser):
 	def reset(self):
-		SGMLParser.reset(self)
+		HTMLParser.reset(self)
 		self.made = []
 		self.urls = []
 		self.text = []
@@ -24,12 +25,28 @@ class Parser(SGMLParser):
 				'.cgi' not in v and '.js' not in v
 		]
 		if href:
+#			print "===>", href
 			self.urls.extend(href)
 
 	def start_link(self, attrs):
 		href = ["%s ++ %s" % (k,v) for k, v in attrs if k=='href']
 		if href:
 			self.made.extend(href)
+
+	def handle_starttag(self, tag, attrs):
+		#attrs = dict(attrs)
+		if tag.lower() == 'a':
+			href = ["%s" % v  for k, v in attrs if k=='href' and
+				'mailto' not in v and
+				'.com' in v and 'acm.org' not in v and
+				'wikipedia' not in v and '.gov' not in v and
+				'.pdf' not in v and '.doc' not in v and
+				'.xls' not in v and '.php' not in v and
+				'.cgi' not in v and '.js' not in v
+			]
+			if href:
+#				print "===>", href
+				self.urls.extend(href)
 
 	def handle_data(self, text):
 		self.text.extend(text)
